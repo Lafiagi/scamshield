6724bd3e902e3a14a289ac109492746bf6c8accd
lafiagi <skillfullmallam@gmail.com>
2025-05-24 17:02:16 +0100

    Modified settings file

--- backend/scamshield/scamshield/settings.py
+++ backend/scamshield/scamshield/settings.py
@@ -89,6 +89,8 @@ WSGI_APPLICATION = "scamshield.wsgi.application"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/scamshield/staticfiles'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
